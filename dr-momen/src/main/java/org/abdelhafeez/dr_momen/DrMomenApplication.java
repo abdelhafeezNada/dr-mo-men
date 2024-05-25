ac7217ea54b699eaa1f84630491131a2080ea513
AbdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-25 18:41:35 +0300

    init

---
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/DrMomenApplication.java
@@ -0,0 +1,13 @@
+package org.abdelhafeez.dr_momen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrMomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrMomenApplication.class, args);
+	}
+
+}
---
+++ dr-momen/src/test/java/org/abdelhafeez/dr_momen/DrMomenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abdelhafeez.dr_momen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrMomenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
