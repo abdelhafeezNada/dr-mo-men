d8cf85d1982d2e675e1ee5c26a9907ff192311fb
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-25 23:06:39 +0300

    implement findAllByName

--- dr-momen/src/main/java/org/abdelhafeez/dr_momen/repo/PatientRepo.java
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/repo/PatientRepo.java
@@ -1,10 +1,14 @@
 package org.abdelhafeez.dr_momen.repo;
 
+import java.util.List;
+
 import org.abdelhafeez.dr_momen.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepo extends JpaRepository<Patient, Long> {
+
+    List<Patient> findAllByName(String name);
 
 }
