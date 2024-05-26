1b26b1fec0b6f592a3760ab55b1238a32465e901
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-26 13:36:39 +0300

    create patient controller

---
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/controller/PatientController.java
@@ -0,0 +1,27 @@
+package org.abdelhafeez.dr_momen.controller;
+
+import org.abdelhafeez.dr_momen.entity.Patient;
+import org.abdelhafeez.dr_momen.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/patients")
+    public ModelAndView listPatients(@RequestParam(defaultValue = "0") int page) {
+        Page<Patient> patientPage = patientService.findPage(page);
+        ModelAndView modelAndView = new ModelAndView("patients");
+        modelAndView.addObject("patients", patientPage.getContent());
+        modelAndView.addObject("currentPage", page);
+        modelAndView.addObject("totalPages", patientPage.getTotalPages());
+        return modelAndView;
+    }
+}
