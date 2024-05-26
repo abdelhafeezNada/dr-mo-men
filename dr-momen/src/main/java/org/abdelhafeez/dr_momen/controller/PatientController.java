697e54ec003df2719074a63fe38fe99f93d3bd5a
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-26 17:11:31 +0300

    create search patient

--- dr-momen/src/main/java/org/abdelhafeez/dr_momen/controller/PatientController.java
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/controller/PatientController.java
@@ -6,10 +6,12 @@ import org.abdelhafeez.dr_momen.entity.Patient;
 import org.abdelhafeez.dr_momen.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,8 +35,16 @@ public class PatientController {
 
     @GetMapping("/patients/new")
     public ModelAndView showCreatePatientForm() {
-        ModelAndView modelAndView = new ModelAndView("new_patient");
+        ModelAndView modelAndView = new ModelAndView("patient_form");
         modelAndView.addObject("patient", new Patient());
+        return modelAndView;
+    }
+
+    @GetMapping("/patients/{id}/edit")
+    public ModelAndView showEditPatientForm(@PathVariable Long id) {
+        Patient patient = patientService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("patient_form");
+        modelAndView.addObject("patient", patient);
         return modelAndView;
     }
 
@@ -50,10 +60,24 @@ public class PatientController {
             return "new_patient";
         }
 
-        patientService.save(patient);
+        if (patient.getId() != null)
+            patientService.update(patient);
+        else
+            patientService.save(patient);
 
         int lastPage = (int) Math.ceil((double) patientService.count() / 10) - 1;
         return "redirect:/patients?page=" + lastPage;
+    }
+
+    @GetMapping("/patients/search")
+    public ModelAndView searchPatients(@RequestParam String name, @RequestParam(defaultValue = "0") int page) {
+        Page<Patient> patientPage = patientService.findByNameContaining(name, page);
+        ModelAndView modelAndView = new ModelAndView("patients");
+        modelAndView.addObject("patients", patientPage.getContent());
+        modelAndView.addObject("currentPage", page);
+        modelAndView.addObject("totalPages", patientPage.getTotalPages());
+        modelAndView.addObject("searchName", name); // Pass the search name to the template
+        return modelAndView;
     }
 
     private boolean isValidPhone(String phone) {
