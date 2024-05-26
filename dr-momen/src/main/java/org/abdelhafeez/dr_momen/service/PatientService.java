9b352423990a19e4ea273554851b2fe7e98f8c87
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-26 13:21:14 +0300

    create patient service

---
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/service/PatientService.java
@@ -0,0 +1,21 @@
+package org.abdelhafeez.dr_momen.service;
+
+import org.abdelhafeez.dr_momen.entity.Patient;
+import org.abdelhafeez.dr_momen.repo.PatientRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class PatientService {
+
+    private final PatientRepo patientRepo;
+
+    public Page<Patient> findPage(int pageNum) {
+        return patientRepo.findAll(PageRequest.of(pageNum, 10));
+    }
+
+}
