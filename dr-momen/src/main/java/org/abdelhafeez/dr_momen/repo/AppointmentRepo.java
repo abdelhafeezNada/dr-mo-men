d070efd6dea120b5e88201da5c315d93f6a88ee4
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-27 12:04:37 +0300

    implement findAllByPatientId

--- dr-momen/src/main/java/org/abdelhafeez/dr_momen/repo/AppointmentRepo.java
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/repo/AppointmentRepo.java
@@ -3,6 +3,8 @@ package org.abdelhafeez.dr_momen.repo;
 import java.util.List;
 
 import org.abdelhafeez.dr_momen.entity.Appointment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import org.springframework.stereotype.Repository;
 public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
 
     List<Appointment> findAllByPatientId(Long id);
+
+    Page<Appointment> findAllByPatientId(Long id, Pageable pageable);
 }
