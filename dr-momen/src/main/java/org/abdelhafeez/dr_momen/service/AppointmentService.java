45a34eb049d5dcab29bbd1ad978d69aa3a850c9d
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-27 12:05:04 +0300

    implement AppointmentService

--- dr-momen/src/main/java/org/abdelhafeez/dr_momen/service/AppointmentService.java
+++ dr-momen/src/main/java/org/abdelhafeez/dr_momen/service/AppointmentService.java
@@ -1,8 +1,24 @@
 package org.abdelhafeez.dr_momen.service;
 
+import org.abdelhafeez.dr_momen.entity.Appointment;
+import org.abdelhafeez.dr_momen.repo.AppointmentRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class AppointmentService {
 
+    private final AppointmentRepo appointmentRepo;
+
+    public Page<Appointment> findByPatientId(Long patientId, int page) {
+        return appointmentRepo.findAllByPatientId(patientId, PageRequest.of(page, 10));
+    }
+
+    public Page<Appointment> findAll(int page) {
+        return appointmentRepo.findAll(PageRequest.of(page, 10));
+    }
 }
